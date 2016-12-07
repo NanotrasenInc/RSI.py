d5c3831e9859a13db470eeb0b8ad273d99d5c671
PJB3005 <pieterjan.briers@gmail.com>
2016-12-07 21:02:07 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="RSI.py",
+    version="1.0",
+    description="A library for manipulation of the RSI format used in YASSS13RTWCF.",
+    url="https://github.com/NanotrasenInc/RSI.py",
+    author="Pieter-Jan Briers",
+    author_email="pieterjan.briers@gmail.com",
+    license="MIT",
+    packages=["rsi"]
+)
